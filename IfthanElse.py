6c4ec20014b99d5081989a49df99907f200080bb
tannerru <tanner.rusher@outlook.com>
2019-03-27 15:53:30 -0500

    Added Elif

--- IfthanElse.py
+++ IfthanElse.py
@@ -5,5 +5,8 @@ age = int(rawage)
 if age>= 20:
         print('You are allowed in!')
         print('What would you like to drink?')
+elif age >= 18:
+    print('You are allowed in')
+    print("But you are not allowed to drink, please feel free to advance")
 else:
     print('Too bad',name, "you are not allowed in.")
